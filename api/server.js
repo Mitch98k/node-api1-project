b2ac6e284b10247e3f4c164332be746b93342896
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-03-10 20:30:12 -0700

    Get requests built and tested, working perfectly.

--- api/server.js
+++ api/server.js
@@ -20,5 +20,29 @@ server.post('/api/users', async (req, res) => {
             res.status(500).json({ message: 'There was an error while saving the user to the database'})
         }
     }
-})
+});
+
+// get requests
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ message: 'The users information could not be retrieved' })
+    }
+});
+server.get('/api/users/:id', async (req, res) => {
+    const {id} = req.params;
+
+        try {
+            const user = await User.findById(id);
+            if (!user) {
+                res.status(404).json({ message: 'the user with the specified ID does not exist'});
+            } else {
+            res.status(200).json(user);
+            }
+        } catch (err) {
+            res.status(500).json({ message: 'the user information could not be retrieved'});
+        }
+});
 module.exports = server; // EXPORT YOUR SERVER instead of {}

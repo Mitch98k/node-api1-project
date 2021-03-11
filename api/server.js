cdd40c5a38b333c881e716c24c76fdd7f76b5971
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-03-10 21:17:23 -0700

    Delete request created and tested, working perfectly.

--- api/server.js
+++ api/server.js
@@ -67,4 +67,20 @@ server.put('/api/users/:id', async (req, res) => {
     }
 });
 
+// delete request
+server.delete('/api/users/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const deletedUser = await User.remove(id);
+        if (!deletedUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist"});
+        } else {
+            res.status(200).json(deletedUser);
+        }
+    } catch (err) {
+        res.status(500).json({ message: "The user could not be removed" });
+    }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}

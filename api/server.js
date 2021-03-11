ef3af40edde0b72c05ec3ae4154965e7c00b08f8
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-03-10 20:55:34 -0700

    Put request created and tested, working perfectly.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-const User = require("./users/model.js");
+const User = require('./users/model.js');
 
 const server = express();
 
@@ -11,13 +11,13 @@ server.post('/api/users', async (req, res) => {
     const user = req.body;
 
     if (!user.name || !user.bio) {
-        res.status(400).json({ message: 'Please privide name and bio for the user' });
+        res.status(400).json({ message: "Please privide name and bio for the user" });
     } else {
         try {
             const newUser = await User.insert(user);
             res.status(201).json(newUser);
         } catch (err) {
-            res.status(500).json({ message: 'There was an error while saving the user to the database'})
+            res.status(500).json({ message: "There was an error while saving the user to the database"})
         }
     }
 });
@@ -28,7 +28,7 @@ server.get('/api/users', async (req, res) => {
         const users = await User.find();
         res.status(200).json(users);
     } catch (err) {
-        res.status(500).json({ message: 'The users information could not be retrieved' })
+        res.status(500).json({ message: "The users information could not be retrieved" })
     }
 });
 server.get('/api/users/:id', async (req, res) => {
@@ -37,12 +37,34 @@ server.get('/api/users/:id', async (req, res) => {
         try {
             const user = await User.findById(id);
             if (!user) {
-                res.status(404).json({ message: 'the user with the specified ID does not exist'});
+                res.status(404).json({ message: "the user with the specified ID does not exist"});
             } else {
             res.status(200).json(user);
             }
         } catch (err) {
-            res.status(500).json({ message: 'the user information could not be retrieved'});
+            res.status(500).json({ message: "the user information could not be retrieved"});
         }
 });
+
+// put request
+server.put('/api/users/:id', async (req, res) => {
+    const {id} = req.params;
+    const user = req.body;
+
+    if (!user.name || !user.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" });
+    } else {
+        try {
+            const updatedUser = await User.update(id, user);
+            if (!updatedUser) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
+            } else {
+                res.status(200).json(updatedUser);
+            }
+        } catch (err) {
+            res.status(500).json({ message: "The user information could not be modified"});
+        }
+    }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
